9e2f192b9f3dcef7dbffed72afe1ac11826054ee
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-04-08 23:13:14 -0700

    Remove chest check for swapping back to inven

--- Common/Players/BendingStancePlayer.cs
+++ Common/Players/BendingStancePlayer.cs
@@ -85,8 +85,8 @@ namespace TerraBend.Common.Players {
         }
 
         public override void PreUpdate() {
-            //If a player opens a chest or has an item on their mouse, swap back to normal inventory
-            currentElementStance = Player.chest > -1 || !Main.mouseItem.IsAir ? null : currentElementStance;
+            //If a player has an item on their mouse, swap back to normal inventory
+            currentElementStance = !Main.mouseItem.IsAir ? null : currentElementStance;
         }
 
         public override void Initialize() {
